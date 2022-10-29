2fb97ca2b12edf457da95f350d91e5539920f814
Ichikyube <ichikyube@gmail.com>
2022-10-29 23:37:36 +0700

    ValidParantheses valid

--- ValidParantheses.js
+++ ValidParantheses.js
@@ -13,25 +13,24 @@
     close: ']'
 }]
 var isValid = function (s) {
-    let arr = [],
-        str = s.split("")
-    while (str.length != 0) {
-        if (parantheses.some(el => el.open == str[0])) {
+    let arr = [], str = s.split("");
+    while(str.length>1) {
+        if (parantheses.some(el => el.open == str[0])==false) return false;
+        while (parantheses.some(el => el.open == str[0])) {
             arr.push(parantheses.findIndex(el => el.open == str[0]))
             str.shift()
-        } else {
-            while (arr.length != 0) {
-                if (str[0] == parantheses[arr[arr.length - 1]].close) {
-                    str.shift()
-                    arr.pop()
-                } else if (parantheses.some(el => el.close == str[0]) && 
-                        str[0] != parantheses[arr[arr.length - 1]].close) {
-                    return false
-                } else break
-            }
         }
-    }
-    if(arr.length>0) return false
-    else return true
+        while (arr.length != 0) {
+            if (str[0] == parantheses[arr[arr.length - 1]].close) {
+                str.shift()
+                arr.pop()
+            } else if (parantheses.some(el => el.close == str[0]) && 
+                    str[0] != parantheses[arr[arr.length - 1]].close) {
+                return false
+            } else break
+        }
+    } 
+    if(str.length == 0 && arr.length == 0) return true
+    else return false
 };
-console.log(isValid(")"))
+console.log(isValid("{"))
